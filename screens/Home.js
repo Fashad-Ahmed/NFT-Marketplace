d337060404aa805b0ebc00c268abf44993d05fad
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-08-28 01:14:34 +0500

    Folder and Files cleanup

--- screens/Home.js
+++ screens/Home.js
@@ -5,55 +5,58 @@ import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
-  const [nftData, setNftData] = useState(NFTData);
-
-  const handleSearch = (value) => {
-    if (value.length === 0) {
-      setNftData(NFTData);
-    }
-
-    const filteredData = NFTData.filter((item) =>
-      item.name.toLowerCase().includes(value.toLowerCase())
-    );
-
-    if (filteredData.length === 0) {
-      setNftData(NFTData);
-    } else {
-      setNftData(filteredData);
-    }
-  };
-
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <FocusedStatusBar backgroundColor={COLORS.primary} />
-      <View style={{ flex: 1 }}>
-        <View style={{ zIndex: 0 }}>
-          <FlatList
-            data={nftData}
-            renderItem={({ item }) => <NFTCard data={item} />}
-            keyExtractor={(item) => item.id}
-            showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
-          />
-        </View>
-
-        <View
-          style={{
-            position: "absolute",
-            top: 0,
-            bottom: 0,
-            right: 0,
-            left: 0,
-            zIndex: -1,
-          }}
-        >
-          <View
-            style={{ height: 300, backgroundColor: COLORS.primary }} />
-          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+  return <Text>Home</Text>;
 };
+// const Home = () => {
+//   const [nftData, setNftData] = useState(NFTData);
+
+//   const handleSearch = (value) => {
+//     if (value.length === 0) {
+//       setNftData(NFTData);
+//     }
+
+//     const filteredData = NFTData.filter((item) =>
+//       item.name.toLowerCase().includes(value.toLowerCase())
+//     );
+
+//     if (filteredData.length === 0) {
+//       setNftData(NFTData);
+//     } else {
+//       setNftData(filteredData);
+//     }
+//   };
+
+//   return (
+//     <SafeAreaView style={{ flex: 1 }}>
+//       <FocusedStatusBar backgroundColor={COLORS.primary} />
+//       <View style={{ flex: 1 }}>
+//         <View style={{ zIndex: 0 }}>
+//           <FlatList
+//             data={nftData}
+//             renderItem={({ item }) => <NFTCard data={item} />}
+//             keyExtractor={(item) => item.id}
+//             showsVerticalScrollIndicator={false}
+//             ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
+//           />
+//         </View>
+
+//         <View
+//           style={{
+//             position: "absolute",
+//             top: 0,
+//             bottom: 0,
+//             right: 0,
+//             left: 0,
+//             zIndex: -1,
+//           }}
+//         >
+//           <View
+//             style={{ height: 300, backgroundColor: COLORS.primary }} />
+//           <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+//         </View>
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
 
 export default Home;
