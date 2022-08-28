21165c0800d11a2d3202c8ba7359bc42859825be
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-08-28 18:16:31 +0500

    Home page layout structure added

--- screens/Home.js
+++ screens/Home.js
@@ -24,60 +24,33 @@ const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary} />
-      <Text>Home</Text>
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={nftData}
+            renderItem={({ item }) => <Text> {item.name} </Text>}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false} // remove the scroll bar
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
-// const Home = () => {
-//   const [nftData, setNftData] = useState(NFTData);
-
-//   const handleSearch = (value) => {
-//     if (value.length === 0) {
-//       setNftData(NFTData);
-//     }
-
-//     const filteredData = NFTData.filter((item) =>
-//       item.name.toLowerCase().includes(value.toLowerCase())
-//     );
-
-//     if (filteredData.length === 0) {
-//       setNftData(NFTData);
-//     } else {
-//       setNftData(filteredData);
-//     }
-//   };
-
-//   return (
-//     <SafeAreaView style={{ flex: 1 }}>
-//       <FocusedStatusBar backgroundColor={COLORS.primary} />
-//       <View style={{ flex: 1 }}>
-//         <View style={{ zIndex: 0 }}>
-//           <FlatList
-//             data={nftData}
-//             renderItem={({ item }) => <NFTCard data={item} />}
-//             keyExtractor={(item) => item.id}
-//             showsVerticalScrollIndicator={false}
-//             ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
-//           />
-//         </View>
-
-//         <View
-//           style={{
-//             position: "absolute",
-//             top: 0,
-//             bottom: 0,
-//             right: 0,
-//             left: 0,
-//             zIndex: -1,
-//           }}
-//         >
-//           <View
-//             style={{ height: 300, backgroundColor: COLORS.primary }} />
-//           <View style={{ flex: 1, backgroundColor: COLORS.white }} />
-//         </View>
-//       </View>
-//     </SafeAreaView>
-//   );
-// };
 
 export default Home;
