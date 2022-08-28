e5d6885178e73265bf2287c485f51146a2386437
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-08-28 13:50:35 +0500

    starting bundling code

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, SafeAreaView, FlatList } from "react-native";
+
+const FocusedStatusBar = () => {
+  return (
+    <View>
+      <Text>FocusedStatusBar</Text>
+    </View>
+  );
+};
+
+export default FocusedStatusBar;
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
-import { View, SafeAreaView, FlatList } from "react-native";
+import { View, Text, SafeAreaView, FlatList } from "react-native";
 
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
 };
 // const Home = () => {
 //   const [nftData, setNftData] = useState(NFTData);
